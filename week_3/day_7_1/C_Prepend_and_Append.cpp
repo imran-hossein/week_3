1b824b3c2cd2b758e72dd0c11cfb1806e34d1935
imran-hossein <mdimranhossein.bd@gmail.com>
2025-03-01 23:38:31 +0600

    Add files via upload
    week 3 added

---
+++ week_3/day_7_1/C_Prepend_and_Append.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;cin >> t;
+    while (t--)
+    {
+        int n;cin >> n;
+        string s;cin >> s;
+        int l = 0, r = n - 1;
+        int ans = n;
+        while (l < r)
+        {
+            if (s[l] == s[r]) break;
+            else ans -= 2;
+            l++;r--;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
